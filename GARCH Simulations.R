d74c8f2a9d0dfcae7bdc856e918ad8ec13a46df7
CosmiCepheid <100585459+CosmiCepheid@users.noreply.github.com>
2023-01-22 14:38:34 +0100

    GARCH coefficients estimations
    they're bad

--- GARCH Simulations.R
+++ GARCH Simulations.R
@@ -1,8 +1,10 @@
 #install.packages("fGarch")
 #install.packages('bootUR')
+#install.packages("rugarch")
 
 library(fGarch)
 library(bootUR)
+library(rugarch)
 rm(list = ls())
 
 seed <- 123
@@ -13,8 +15,10 @@ GARCHcoeff <- function(p,q,upbound){
   b <- rep(0,q)
   a[1] <- runif(1, 0, upbound) #initalises the first "a" coefficient
   #upper bound so the first coefficient isn't excessively large compared to the others
-  for (i in 2:p){
-    a[i] <- runif(1, 0, (1-sum(a))) #generated such that the sum of the coefficient stay < 1
+  if (p > 1){
+    for (i in 2:p){
+      a[i] <- runif(1, 0, (1-sum(a))) #generated such that the sum of the coefficient stay < 1
+    }
   }
   for (i in 1:q){
     b[i] <- runif(1, 0, (1-(sum(a))+sum(b))) 
@@ -23,8 +27,8 @@ GARCHcoeff <- function(p,q,upbound){
 return(list(a,b))
 }
 
-GARCHcoeff(2,2,0.8)
-
+#GARCHcoeff(2,2,0.8)
+trueCoef_11 <- GARCHcoeff(1,1,1)
 
 #generates n datapoints following a GARCH(p,q) with a list of coefficients coeff
 #omega is by default
@@ -36,7 +40,7 @@ simulationGARCH <- function(coeff,n){
 }
 
 #example with the generated coefficients
-sim_series <- simulationGARCH(GARCHcoeff(2,2,0.8),1000)
+simSeries <- simulationGARCH(trueCoef_11,1000)
 #no trend, looks stationary
 stationarity <- function(series){
   adftest <- adf(series, deterministics = "intercept")
@@ -48,8 +52,20 @@ stationarity <- function(series){
   }
 }
 
-stationarity(sim_series)
+stationarity(simSeries)
 
 #example with own coefficents
 #simulationGARCH(list(alpha = c(0.2, 0.05), beta = c(0.09,0.02)),20)
+
+
+#find the best lag order for a GARCH model (to do later, first GARCH(1,1))
+
+#estimate the parameters of a simple GARCH(1,1)
+garchSpec_11 <- ugarchspec(variance.model = list(garchOrder = c(1, 1)), 
+                           mean.model = list(armaOrder = c(0, 0)))
+#armaOrder = c(0, 0) because the mean equation doesn't have ARMA components
+garchFit_11 <- ugarchfit(data = simSeries, spec = garchSpec_11)
+#can have problems converging (??)
+coef(garchFit_11) #estimates are horrendous
+
 
